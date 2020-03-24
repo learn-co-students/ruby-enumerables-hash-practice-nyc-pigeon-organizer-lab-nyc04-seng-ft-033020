4962f3ec2ac850cefb3ec35a81fdaf9c6932063f
jiangmichael1 <jiangm1@yahoo.com>
2020-03-24 07:46:44 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pgn_hash = {}
+  x = 0
+
+  data.each do |k, v|
+    v.each do |new_value, names|
+      names.each do |name|
+
+        if !pgn_hash[name]
+          pgn_hash[name] = {}
+        end
+
+        if !pgn_hash[name][k]
+          pgn_hash[name][k] = []
+        end
+
+        pgn_hash[name][k] << new_value.to_s
+      end
+    end
+  end
+  pgn_hash
 end
+
+
